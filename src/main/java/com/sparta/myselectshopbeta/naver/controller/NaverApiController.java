e9b306b21af272c6a38fb637f0f461089c2b254c
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-08 02:11:55 +0900

    Day25_230208_Setting

---
+++ src/main/java/com/sparta/myselectshopbeta/naver/controller/NaverApiController.java
@@ -0,0 +1,24 @@
+package com.sparta.myselectshopbeta.naver.controller;
+
+import com.sparta.myselectshopbeta.naver.dto.ItemDto;
+import com.sparta.myselectshopbeta.naver.service.NaverApiService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class NaverApiController {
+
+    private final NaverApiService naverApiService;
+
+    @GetMapping("/search")
+    public List<ItemDto> searchItems(@RequestParam String query){
+        return naverApiService.searchItems(query);
+    }
+}
