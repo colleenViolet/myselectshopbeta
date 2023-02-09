53c14c282b17af1a608a420832ae7570fde8a4d2
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-09 22:50:30 +0900

    Day25_230208_AllInOneController

--- src/main/java/com/sparta/myselectshopbeta/controller/AllInOneController.java
+++ src/main/java/com/sparta/myselectshopbeta/controller/AllInOneController.java
@@ -3,12 +3,11 @@ package com.sparta.myselectshopbeta.controller;
 import com.sparta.myselectshopbeta.dto.ProductRequestDto;
 import com.sparta.myselectshopbeta.dto.ProductResponseDto;
 import com.sparta.myselectshopbeta.entity.Product;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -50,5 +49,78 @@ public class AllInOneController {
 
         // 응답 보내기
         return new ProductResponseDto(product);
+    }
+
+    // 관심 상품 조회하기
+    @GetMapping("/products")
+    public List<ProductResponseDto> getProducts() throws SQLException {
+        List<ProductResponseDto> products = new ArrayList<>();
+
+        // DB 연결
+        Connection connection = DriverManager.getConnection("jdbc:h2:mem:db", "sa", "");
+
+        // DB Query 작성 및 실행
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from product");
+
+        // DB Query 결과를 상품 객체 리스트로 변환
+        while (rs.next()) {
+            Product product = new Product();
+            product.setId(rs.getLong("id"));
+            product.setImage(rs.getString("image"));
+            product.setLink(rs.getString("link"));
+            product.setLprice(rs.getInt("lprice"));
+            product.setMyprice(rs.getInt("myprice"));
+            product.setTitle(rs.getString("title"));
+            products.add(new ProductResponseDto(product));
+        }
+
+        // DB 연결 해제
+        rs.close();
+        connection.close();
+
+        // 응답 보내기
+        return products;
+    }
+
+    // 관심 상품 최저가 등록하기
+    @PutMapping("/products/{id}")
+    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
+        Product product = new Product();
+        // DB 연결
+        Connection connection = DriverManager.getConnection("jdbc:h2:mem:db", "sa", "");
+
+        // DB Query 작성
+        PreparedStatement ps = connection.prepareStatement("select * from product where id = ?");
+        ps.setLong(1, id);
+
+        // DB Query 실행
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()) {
+            product.setId(rs.getLong("id"));
+            product.setImage(rs.getString("image"));
+            product.setLink(rs.getString("link"));
+            product.setLprice(rs.getInt("lprice"));
+            product.setMyprice(rs.getInt("myprice"));
+            product.setTitle(rs.getString("title"));
+        } else {
+            throw new NullPointerException("해당 아이디가 존재하지 않습니다.");
+        }
+
+        // DB Query 작성
+        ps = connection.prepareStatement("update product set myprice = ? where id = ?");
+        ps.setInt(1, requestDto.getMyprice());
+        ps.setLong(2, product.getId());
+
+        // DB Query 실행
+        ps.executeUpdate();
+
+        // DB 연결 해제
+        rs.close();
+        ps.close();
+        connection.close();
+
+        // 응답 보내기 (업데이트된 상품 id)
+        return product.getId();
     }
 }
