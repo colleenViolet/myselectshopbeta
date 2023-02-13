33401a22a78ba15aa56a21c57acda1033b59589a
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-14 02:27:30 +0900

    Day31_230214

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -3,5 +3,10 @@ package com.sparta.myselectshop.repository;
 import com.sparta.myselectshop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllByUserId(Long userId);
+    Optional<Product> findByIdAndUserId(Long id, Long userId);
 }
