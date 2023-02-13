e406809d94d269ff2672c14fd75f10dcb7199c1e
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-14 02:01:22 +0900

    Day30_230213

---
+++ src/main/java/com/sparta/myselectshop/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.sparta.myselectshop.controller;
+
+import com.sparta.myselectshop.dto.LoginRequestDto;
+import com.sparta.myselectshop.dto.SignupRequestDto;
+import com.sparta.myselectshop.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/signup")
+    public ModelAndView signupPage() {
+        return new ModelAndView("signup");
+    }
+
+    @GetMapping("/login")
+    public ModelAndView loginPage() {
+        return new ModelAndView("login");
+    }
+
+    @PostMapping("/signup")
+    public String signup(SignupRequestDto signupRequestDto) {
+        userService.signup(signupRequestDto);
+        return "redirect:/api/user/login";
+    }
+
+    @PostMapping("/login")
+    public String login(LoginRequestDto loginRequestDto) {
+        userService.login(loginRequestDto);
+        return "redirect:/api/shop";
+    }
+
+}
